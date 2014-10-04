48c654fdc273e5b42d139b9b34c5a93165882a18
Meik Michalke <meik.michalke@uni-duesseldorf.de>
2014-10-04 22:36:02 +0000

    last bits for a first release of rk.power plugin
    svn path=/branches/external_plugins/; revision=4874

---
+++ rk.power/R/rk.power-package.R
@@ -0,0 +1,23 @@
+#' RKWard GUI to perform power analysis and sample size estimation..
+#'
+#' \tabular{ll}{
+#' Package: \tab rk.power\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.01-1\cr
+#' Date: \tab 2014-10-05\cr
+#' Depends: \tab  rkward (>= 0.5.6)\cr
+#' Enhances: \tab rkward\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab http://rkward.sf.net\cr
+#' }
+#'
+#' RKWard GUI to perform power analysis and sample size estimation.
+#'
+#' @aliases rk.power-package
+#' @name rk.power-package
+#' @docType package
+#' @title The rk.power Package
+#' @author Meik Michalke
+#' @keywords package
+NULL
