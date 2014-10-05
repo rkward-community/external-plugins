e9d0c301ff2379205e373ac29ab6a634d8d72be1
Meik Michalke <meik.michalke@uni-duesseldorf.de>
2014-10-05 21:12:31 +0000

    layout changes done by roxygen
    svn path=/branches/external_plugins/; revision=4877

--- rk.power/R/rk.power-package.R
+++ rk.power/R/rk.power-package.R
@@ -5,7 +5,7 @@
 #' Type: \tab Package\cr
 #' Version: \tab 0.01-2\cr
 #' Date: \tab 2014-10-05\cr
-#' Depends: \tab rkward (>= 0.5.6)\cr
+#' Depends: \tab  rkward (>= 0.5.6)\cr
 #' Enhances: \tab rkward\cr
 #' License: \tab GPL (>= 3)\cr
 #' LazyLoad: \tab yes\cr
